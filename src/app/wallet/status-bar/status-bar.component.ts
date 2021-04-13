123f8ab50457d31d2c93cf98365c76c6727a3d09
dev0tion <pieterjan.vanhoof@stratisplatform.com>
2021-04-13 12:20:04 +0200

    Add estimated blocks left

--- src/app/wallet/status-bar/status-bar.component.ts
+++ src/app/wallet/status-bar/status-bar.component.ts
@@ -16,6 +16,7 @@ export class StatusBarComponent implements OnInit {
   public percentSynced: string;
   public toolTip = '';
   public connectedNodesTooltip = '';
+  private processedText: string;
 
   constructor(
     private nodeService: NodeService,
@@ -27,25 +28,32 @@ export class StatusBarComponent implements OnInit {
     this.generalInfo = this.nodeService.generalInfo()
       .pipe(tap(
         response => {
-          // Don't show if wallet is ahead of chainTip
-          if (response.lastBlockSyncedHeight > response.chainTip) {
-            response.chainTip = response.lastBlockSyncedHeight;
+          if (response) {
+            // Don't show if wallet is ahead of chainTip
+            if (response.lastBlockSyncedHeight > response.chainTip) {
+              response.chainTip = response.lastBlockSyncedHeight;
+            }
+
+            this.percentSynced = (response.percentSynced || 0).toFixed(0) + '%';
+            if (response.isChainSynced) {
+              this.processedText = `Processed ${response.lastBlockSyncedHeight || '0'} out of ${response.chainTip} blocks.`;
+            } else {
+              this.processedText = `Processed ${response.lastBlockSyncedHeight || '0'} out of (estimated) ${response.chainTip} blocks.`;
+            }
+
+            this.toolTip = `Synchronizing. ${this.processedText}`;
+
+            if (response.connectedNodes === 1) {
+              this.connectedNodesTooltip = '1 connection';
+            } else if (response.connectedNodes >= 0) {
+              this.connectedNodesTooltip = `${response.connectedNodes} connections`;
+            }
+
+            if (response.percentSynced === 100) {
+              this.toolTip = `Up to date. ${this.processedText}`;
+            }
           }
-
-          this.percentSynced = (response.percentSynced || 0).toFixed(0) + '%';
-          const processedText = `Processed ${response.lastBlockSyncedHeight || '0'} out of ${response.chainTip} blocks.`;
-
-          this.toolTip = `Synchronizing. ${processedText}`;
-
-          if (response.connectedNodes === 1) {
-            this.connectedNodesTooltip = '1 connection';
-          } else if (response.connectedNodes >= 0) {
-            this.connectedNodesTooltip = `${response.connectedNodes} connections`;
-          }
-
-          if (response.percentSynced === 100) {
-            this.toolTip = `Up to date.  ${processedText}`;
-          }
-        }));
+        }
+      ));
   }
 }
