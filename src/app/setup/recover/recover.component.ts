b9c330ae2878cb17f8630713be2eafbcc0272f5c
dev0tion <pieterjan.vanhoof@stratisplatform.com>
2021-04-13 10:55:48 +0200

    Remove creation date from recovery component

--- src/app/setup/recover/recover.component.ts
+++ src/app/setup/recover/recover.component.ts
@@ -1,60 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
-
 import { GlobalService } from '@shared/services/global.service';
 import { ApiService } from '@shared/services/api.service';
 import { ModalService } from '@shared/services/modal.service';
-
 import { WalletRecovery } from '@shared/models/wallet-recovery';
+import { PasswordValidationDirective } from '@shared/directives/password-validation.directive';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recover',
   templateUrl: './recover.component.html',
   styleUrls: ['./recover.component.css']
 })
-export class RecoverComponent implements OnInit {
+export class RecoverComponent implements OnInit, OnDestroy {
 
   constructor(private globalService: GlobalService, private apiService: ApiService, private genericModalService: ModalService, private router: Router, private fb: FormBuilder) {
     this.buildRecoverForm();
   }
 
   public recoverWalletForm: FormGroup;
-  public creationDate: Date;
   public isRecovering = false;
-  public minDate = new Date("2009-08-09");
-  public maxDate = new Date();
-  public bsConfig: Partial<BsDatepickerConfig>;
   private walletRecovery: WalletRecovery;
+  private formValueChanges$: Subscription;
+  private passphrase$: Subscription;
 
-  ngOnInit() {
-    this.bsConfig = Object.assign({}, {showWeekNumbers: false, containerClass: 'theme-dark-blue'});
-  }
+  ngOnInit(): void { }
 
   private buildRecoverForm(): void {
     this.recoverWalletForm = this.fb.group({
-      "walletName": ["", [
+      walletName: ["", [
+        // eslint-disable-next-line @typescript-eslint/unbound-method
         Validators.required,
         Validators.minLength(1),
         Validators.maxLength(24),
         Validators.pattern(/^[a-zA-Z0-9]*$/)
       ]
       ],
-      "walletMnemonic": ["", Validators.required],
-      "walletDate": ["", Validators.required],
-      "walletPassphrase": [""],
-      "walletPassword": ["", Validators.required],
-      "selectNetwork": ["test", Validators.required]
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      walletMnemonic: ["", Validators.required],
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      walletPassword: ["", Validators.required],
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      walletPasswordConfirmation: ["", Validators.required],
+      walletPassphrase: [""],
+      walletPassphraseConfirmation: [""],
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      selectNetwork: ["test", Validators.required]
+    }, {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      validator: PasswordValidationDirective.MatchPassword
     });
 
-    this.recoverWalletForm.valueChanges
-      .subscribe(data => this.onValueChanged(data));
+    this.formValueChanges$ = this.recoverWalletForm.valueChanges
+      .subscribe(() => this.onValueChanged());
+
+    const passphrase = this.recoverWalletForm.get('walletPassphrase');
+    const passphraseConfirmation = this.recoverWalletForm.get('walletPassphraseConfirmation');
+
+    this.passphrase$ = passphrase.valueChanges.subscribe(rsp => {
+      this.setDynamicPassphraseValidators(rsp, { passphrase, passphraseConfirmation });
+    });
 
     this.onValueChanged();
   }
 
-  onValueChanged(data?: any) {
+  onValueChanged(): void {
     if (!this.recoverWalletForm) { return; }
     const form = this.recoverWalletForm;
     for (const field in this.formErrors) {
@@ -63,71 +74,115 @@ export class RecoverComponent implements OnInit {
       if (control && control.dirty && !control.valid) {
         const messages = this.validationMessages[field];
         for (const key in control.errors) {
-          this.formErrors[field] += messages[key] + ' ';
+          this.formErrors[field] += `${String(messages[key])} `;
         }
       }
     }
   }
 
   formErrors = {
-    'walletName': '',
-    'walletMnemonic': '',
-    'walletDate': '',
-    'walletPassword': '',
-    'walletPassphrase': '',
-
+    walletName: '',
+    walletMnemonic: '',
+    walletPassword: '',
+    walletPasswordConfirmation: '',
+    walletPassphrase: '',
+    walletPassphraseConfirmation: '',
   };
 
   validationMessages = {
-    'walletName': {
+    walletName: {
       'required': 'A wallet name is required.',
       'minlength': 'A wallet name must be at least one character long.',
       'maxlength': 'A wallet name cannot be more than 24 characters long.',
       'pattern': 'Please enter a valid wallet name. [a-Z] and [0-9] are the only characters allowed.'
     },
-    'walletMnemonic': {
+    walletMnemonic: {
       'required': 'Please enter your 12 word phrase.'
     },
-    'walletDate': {
-      'required': 'Please choose the date the wallet should sync from.'
-    },
-    'walletPassword': {
+    walletPassword: {
       'required': 'A password is required.'
     },
-
+    walletPasswordConfirmation: {
+      required: 'Confirm your password.',
+      walletPasswordConfirmation: 'Passwords do not match.'
+    },
+    walletPassphraseConfirmation: {
+      required: 'Confirm your passphrase.',
+      walletPassphraseConfirmation: 'Passphrases do not match.'
+    }
   };
 
-  public onBackClicked() {
+  public onBackClicked(): void {
     this.router.navigate(["/setup"]);
   }
 
-  public onRecoverClicked(){
+  public onRecoverClicked(): void {
     this.isRecovering = true;
-
-    const recoveryDate = new Date(this.recoverWalletForm.get("walletDate").value);
-    recoveryDate.setDate(recoveryDate.getDate() - 1);
 
     this.walletRecovery = new WalletRecovery(
       this.recoverWalletForm.get("walletName").value,
       this.recoverWalletForm.get("walletMnemonic").value,
       this.recoverWalletForm.get("walletPassword").value,
-      this.recoverWalletForm.get("walletPassphrase").value,
-      recoveryDate
+      this.recoverWalletForm.get("walletPassphrase").value
     );
     this.recoverWallet(this.walletRecovery);
   }
 
-  private recoverWallet(recoverWallet: WalletRecovery) {
+  private recoverWallet(recoverWallet: WalletRecovery): void {
     this.apiService.recoverStratisWallet(recoverWallet)
-      .subscribe(
-        response => {
+      .toPromise()
+      .then(
+        () => {
           const body = "Your wallet has been recovered. \nYou will be redirected to the decryption page.";
           this.genericModalService.openModal("Wallet Recovered", body);
           this.router.navigate(['']);
         },
-        error => {
+        () => {
           this.isRecovering = false;
         }
       );
+  }
+
+  private setDynamicPassphraseValidators(passphraseValue: string, controls: { passphrase: AbstractControl; passphraseConfirmation: AbstractControl }): void {
+    const { passphrase, passphraseConfirmation } = controls;
+
+    if (passphraseValue.length) {
+      // passphrase and confirmation should be required if passphrase is not null
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      passphrase.setValidators(Validators.required);
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      passphraseConfirmation.setValidators(Validators.required);
+
+      // Update form group validators to include MatachPassword and MatchPassphrase
+      this.recoverWalletForm.setValidators([
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        PasswordValidationDirective.MatchPassword,
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        PasswordValidationDirective.MatchPassphrase
+      ]);
+    } else { // Else, passphrase field is null, clear validators
+      // clear passphrase validators, errors, mark pristine
+      passphrase.clearValidators();
+      passphrase.setErrors(null);
+      passphrase.markAsPristine();
+
+      // clear confirmation validators, errors, mark pristine
+      passphraseConfirmation.setValue(null);
+      passphraseConfirmation.clearValidators();
+      passphraseConfirmation.setErrors(null);
+      passphraseConfirmation.markAsPristine();
+
+      // clear then set MatchPassword validator on form
+      this.recoverWalletForm.clearValidators();
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      this.recoverWalletForm.setValidators(PasswordValidationDirective.MatchPassword);
+    }
+
+    this.recoverWalletForm.updateValueAndValidity();
+  }
+
+  public ngOnDestroy(): void {
+    this.formValueChanges$.unsubscribe();
+    this.passphrase$.unsubscribe();
   }
 }
