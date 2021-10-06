83c759595e0219ffea40a0502b3723376b642c5a
Francois de la Rouviere <fassadlr@gmail.com>
2021-10-06 15:36:32 +0100

    Bump version to 1.7.0. (#45)

--- src/app/shared/services/global.service.ts
+++ src/app/shared/services/global.service.ts
@@ -15,7 +15,7 @@ export class GlobalService {
     this.setDaemonIP();
   }
 
-  private applicationVersion = '1.6.1';
+  private applicationVersion = '1.7.0';
   private gitCommit = "";
   private testnet = false;
   private cirrusMainApiPort = 37223;
